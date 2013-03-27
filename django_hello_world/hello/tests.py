9a43b520d91d10628f132976bb1c8b26b5aa0d4b
adrekoval@gmail.com <adrekoval@gmail.com>
2013-03-27 03:29:15 +0200

    ticket:1 Finished

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -11,14 +11,6 @@ from django.test.client import Client
 from django_hello_world.hello.models import Person
 
 
-class HelloTest(TestCase):
-
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
 class HttpTest(TestCase):
     def setUp(self):
         self.me = Person.objects.get(id=1)
