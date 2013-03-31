e598d176d8e5d8193a04918caec614423fb43c5f
adrekoval@gmail.com <adrekoval@gmail.com>
2013-03-31 19:58:51 +0300

    ticket:1 Made Selenium work with driver instead of browser

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -5,6 +5,7 @@ from hello.models import Person
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class HttpTest(TestCase):
@@ -22,18 +23,20 @@ class HttpTest(TestCase):
 
 
 class HttpTestSelenium(LiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-        self.me = Person.objects.get(id=1)
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = WebDriver()
+        super(HttpTestSelenium, cls).setUpClass()
 
-    def tearDown(self):
-        self.browser.quit()
+    @classmethod
+    def tearDownClass(cls):
+        super(HttpTestSelenium, cls).tearDownClass()
+        cls.driver.quit()
     
 
     def test_home(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element_by_tag_name('body')
+        self.driver.get(self.live_server_url)
+        body = self.driver.find_element_by_tag_name('body')
 
         self.assertIn(self.me.name, body.text)
         self.assertIn(self.me.bio, body.text)
@@ -41,19 +44,19 @@ class HttpTestSelenium(LiveServerTestCase):
 
     
     def test_check_admin_work_and_contains_Person(self):
-        self.browser.get(self.live_server_url + '/admin/')
-        body = self.browser.find_element_by_tag_name('body')
+        self.driver.get(self.live_server_url + '/admin/')
+        body = self.driver.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)#checks admin is up
 
-        username_field = self.browser.find_element_by_name('username')
+        username_field = self.driver.find_element_by_name('username')
         username_field.send_keys('admin')
-        password_field = self.browser.find_element_by_name('password')
+        password_field = self.driver.find_element_by_name('password')
         password_field.send_keys('admin')
         password_field.send_keys(Keys.RETURN)
-        body = self.browser.find_element_by_tag_name('body')
+        body = self.driver.find_element_by_tag_name('body')
         self.assertIn('Site administration', body.text)#checks you can login
 
-        polls_links = self.browser.find_elements_by_link_text('Person')
+        polls_links = self.driver.find_elements_by_link_text('Person')
         self.assertEquals(len(polls_links), 2)#checks that Person is added to admin
 
        
