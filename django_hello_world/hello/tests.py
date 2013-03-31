288eb632df088946f29ecf429f4487c260d67bd9
adrekoval@gmail.com <adrekoval@gmail.com>
2013-03-31 11:02:36 +0300

    ticket:1 Updated tests(Selenium)

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -1,23 +1,11 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 from hello.models import Person
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-
-class HelloTest(TestCase):
-
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
 
 class HttpTest(TestCase):
     def setUp(self):
@@ -30,4 +18,43 @@ class HttpTest(TestCase):
         self.assertContains(response,self.me.bio)
         self.assertContains(response,self.me.email)
 
+
+
+
+class HttpTestSelenium(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        self.me = Person.objects.get(id=1)
+
+    def tearDown(self):
+        self.browser.quit()
+    
+
+    def test_home(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element_by_tag_name('body')
+
+        self.assertIn(self.me.name, body.text)
+        self.assertIn(self.me.bio, body.text)
+        self.assertIn(self.me.email, body.text)
+
+    
+    def test_check_admin_work_and_contains_Person(self):
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)#checks admin is up
+
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)#checks you can login
+
+        polls_links = self.browser.find_elements_by_link_text('Person')
+        self.assertEquals(len(polls_links), 2)#checks that Person is added to admin
+
+       
 
--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,16 +1,10 @@
 from django.contrib.auth.models import User
 from hello.models import Person
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import Http404
-from django.template import RequestContext
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    try:
-        person = Person.objects.get(id=1)
-    except Person.DoesNotExist:
-        raise Http404    
-    #person = get_object_or_404(Person, id=1)
-    return render_to_response("hello/index.html", {"person": person}, RequestContext(request))
+    person = get_object_or_404(Person, id=1)
+    return render(request, "hello/index.html", {"person": person})
 
 
 
